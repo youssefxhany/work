35579184d55c82af506310900448ae9afad6927e
youssef hany <youssef.hossam2435@gmail.com>
2022-04-21 23:11:02 +0200

    stack added to be able to implement iterative solution for traversals

--- algo_ds_c++/implementation/Trees/treeTraversals.cpp
+++ algo_ds_c++/implementation/Trees/treeTraversals.cpp
@@ -6,6 +6,49 @@
 #define COUNT 10
 using namespace std;
 
+
+template<class T>
+struct LinkedListNode {
+    T value;
+    struct LinkedListNode<T>* next;
+    struct LinkedListNode<T>* prev;
+    LinkedListNode(T value);
+    LinkedListNode();
+};
+
+template<class T>
+class LinkedList {
+private:
+    struct LinkedListNode<T>* head, *tail;
+    int capacity = 0;
+public:
+    LinkedList();
+    LinkedList(T value);
+    void append(T value);
+    struct LinkedListNode<T>* removeLast();
+    struct LinkedListNode<T>* getTail();
+    int getSize();
+    void printForward();
+    void printBackward();
+private:
+    struct LinkedListNode<T>* allocateLinkedListNode(T value);
+    void deallocateLinkedListNode(struct LinkedListNode<T>* node);
+};
+
+template<class T>
+class Stack {
+private:
+    LinkedList<T> stack;
+public:
+    Stack();
+    Stack(T value);
+    void push(T value);
+    T pop();
+    T top();
+    bool isEmpty();
+    void print();
+};
+
 template<class T>
 struct Node {
     T value;
@@ -23,15 +66,15 @@ public:
     BinarySearchTree();
     void insert(T value);
     struct Node<T>* lookup(T value);
-    void inorder();
-    void preorder();
-    void postorder();
+    void inorderRecursive();
+    void preorderRecursive();
+    void postorderRecusrsive();
     void print();
     ~BinarySearchTree();
 private:
-    T* inorderTraversal(struct Node<T>* node, T* traversal);
-    T* preorderTraversal(struct Node<T>* node, T* traversal);
-    T* postorderTraversal(struct Node<T>* node, T* traversal);
+    T* inorderRecursiveTraversal(struct Node<T>* node, T* traversal);
+    T* preorderRecursiveTraversal(struct Node<T>* node, T* traversal);
+    T* postorderRecursiveTraversal(struct Node<T>* node, T* traversal);
     void printTree(struct Node<T>* root, int space);
     struct Node<T>* getRoot();
     struct Node<T>* allocateNode(T value);
@@ -69,13 +112,153 @@ int main()
     binarySearchTree.lookup(1000);
 
     cout << "--------------------------------------------------------" << endl;
-
-    binarySearchTree.inorder();
-    binarySearchTree.preorder();
-    binarySearchTree.postorder();
+    cout << "RUCURSIVE TRAVERSALS:" << endl;
+    cout << "----------------------" << endl;
+    binarySearchTree.inorderRecursive();
+    binarySearchTree.preorderRecursive();
+    binarySearchTree.postorderRecusrsive();
 
     cout << "--------------------------------------------------------" << endl;
+
 }
+
+//stack methods
+
+template<class T>
+Stack<T>::Stack() : stack() {
+
+}
+
+template<class T>
+Stack<T>::Stack(T value) : stack(value) {
+}
+
+template<class T>
+void Stack<T>::push(T value) {
+    this->stack.append(value);
+}
+
+template<class T>
+T Stack<T>::pop() {
+    if (this->isEmpty()) {
+        cout << "CANT'T POP FROM EMPTY STACK" << endl;
+        return NULL;
+    }
+    return this->stack.removeLast()->value;
+}
+
+template<class T>
+T Stack<T>::top() {
+    if (this->isEmpty()) {
+        cout << "NO TOP FOR EMPTY STACK" << endl;
+        return NULL;
+    }
+    return this->stack.getTail()->value;
+}
+
+template<class T>
+bool Stack<T>::isEmpty() {
+    if (this->stack.getSize() == 0)
+        return true;
+    return false;
+}
+
+template<class T>
+void Stack<T>::print() {
+    this->stack.printForward();
+}
+
+//linked list node constructors
+
+template<class T>
+LinkedListNode<T>::LinkedListNode() {
+}
+
+template<class T>
+LinkedListNode<T>::LinkedListNode(T value) {
+    this->value = value;
+    this->next = nullptr;
+    this->prev = nullptr;
+}
+
+//linked list methods
+
+template<class T>
+LinkedList<T>::LinkedList() {
+    this->head = this->tail = nullptr;
+}
+
+template<class T>
+LinkedList<T>::LinkedList(T value) {
+    struct LinkedListNode<T>* node = head = tail = allocateLinkedListNode(value);
+}
+
+template<class T>
+void LinkedList<T>::append(T value) {
+    struct LinkedListNode<T>* node = allocateLinkedListNode(value);
+    if (head == nullptr && tail == nullptr) {
+        head = tail = node;
+    }
+    tail->next = node;
+    node->prev = tail;
+    tail = node;
+}
+
+template<class T>
+LinkedListNode<T>* LinkedList<T>::removeLast() {
+    struct LinkedListNode<T>* removedNode = tail;
+    struct LinkedListNode<T>* temp = new LinkedListNode<T>(removedNode->value);
+    (removedNode->prev)->next = NULL;
+    tail = removedNode->prev;
+    deallocateLinkedListNode(removedNode);
+    return temp;
+}
+
+template<class T>
+LinkedListNode<T>* LinkedList<T>::getTail() {
+    return this->tail;
+}
+
+template<class T>
+int LinkedList<T>::getSize() {
+    return this->capacity;
+}
+
+template<class T>
+void LinkedList<T>::printForward() {
+    struct LinkedListNode<T>* start = head;
+    for (int i = 0;i < capacity;i++) {
+        cout << "[" << start->value << "]<-->";
+        start = start->next;
+    }
+    cout << endl;
+}
+
+template<class T>
+void LinkedList<T>::printBackward() {
+    struct LinkedListNode<T>* node = tail;
+    for (int i = 0;i < capacity;i++) {
+        cout  << "[" << node->value << "]<-->";
+        node = node->prev;
+    }
+    cout << endl;
+}
+
+template<class T>
+struct LinkedListNode<T>* LinkedList<T>::allocateLinkedListNode(T value) {
+    struct LinkedListNode<T>* node = new LinkedListNode<T>(value);
+    this->capacity++;
+    return node;
+}
+
+template<class T>
+void LinkedList<T>::deallocateLinkedListNode(struct LinkedListNode<T>* node) {
+    delete node;
+    this->capacity--;
+    node = NULL;
+}
+
+//binary search tree methods
 
 template<class T>
 BinarySearchTree<T>::BinarySearchTree() {
@@ -158,9 +341,9 @@ struct Node<T>* BinarySearchTree<T>::lookup(T value) {
 }
 
 template<class T>
-void BinarySearchTree<T>::inorder() {
+void BinarySearchTree<T>::inorderRecursive() {
     T* traversal = new T[this->capacity];
-    this->inorderTraversal(this->getRoot(), &traversal[0]);
+    this->inorderRecursiveTraversal(this->getRoot(), &traversal[0]);
     cout << "INORDER TRAVERSAL: ";
     for (int i = 0;i < this->capacity;i++) {
         cout << traversal[i] << "--->";
@@ -169,9 +352,9 @@ void BinarySearchTree<T>::inorder() {
 }
 
 template<class T>
-void BinarySearchTree<T>::preorder() {
+void BinarySearchTree<T>::preorderRecursive() {
     T* traversal = new T[this->capacity];
-    this->preorderTraversal(this->getRoot(), traversal);
+    this->preorderRecursiveTraversal(this->getRoot(), traversal);
     cout << "PREORDER TRAVERSAL: ";
     for (int i = 0;i < this->capacity;i++) {
         cout << traversal[i] << "--->";
@@ -180,9 +363,9 @@ void BinarySearchTree<T>::preorder() {
 }
 
 template<class T>
-void BinarySearchTree<T>::postorder() {
+void BinarySearchTree<T>::postorderRecusrsive() {
     T* traversal = new T[this->capacity];
-    this->postorderTraversal(this->getRoot(), traversal);
+    this->postorderRecursiveTraversal(this->getRoot(), traversal);
     cout << "POSTORDER TRAVERSAL: ";
     for (int i = 0;i < this->capacity;i++) {
         cout << traversal[i] << "--->";
@@ -201,38 +384,38 @@ BinarySearchTree<T>::~BinarySearchTree() {
 }
 
 template<class T>
-T* BinarySearchTree<T>::inorderTraversal(struct Node<T>* node,T* traversal) {
+T* BinarySearchTree<T>::inorderRecursiveTraversal(struct Node<T>* node,T* traversal) {
     int static i = 0;
 
     if (node == NULL)
         return NULL;
 
-    inorderTraversal(node->left, traversal);
+    inorderRecursiveTraversal(node->left, traversal);
     traversal[i++] = node->value;
-    inorderTraversal(node->right, traversal);
+    inorderRecursiveTraversal(node->right, traversal);
 }
 
 template<class T>
-T* BinarySearchTree<T>::preorderTraversal(struct Node<T>* node, T* traversal) {
+T* BinarySearchTree<T>::preorderRecursiveTraversal(struct Node<T>* node, T* traversal) {
     int static i = 0;
 
     if (node == NULL)
         return NULL;
 
     traversal[i++] = node->value;
-    preorderTraversal(node->left, traversal);
-    preorderTraversal(node->right, traversal);
+    preorderRecursiveTraversal(node->left, traversal);
+    preorderRecursiveTraversal(node->right, traversal);
 }
 
 template<class T>
-T* BinarySearchTree<T>::postorderTraversal(struct Node<T>* node, T* traversal) {
+T* BinarySearchTree<T>::postorderRecursiveTraversal(struct Node<T>* node, T* traversal) {
     int static i = 0;
 
     if (node == NULL)
         return NULL;
 
-    postorderTraversal(node->left, traversal);
-    postorderTraversal(node->right, traversal);
+    postorderRecursiveTraversal(node->left, traversal);
+    postorderRecursiveTraversal(node->right, traversal);
     traversal[i++] = node->value;
 }
 
